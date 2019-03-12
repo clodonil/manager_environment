1949295f85ec5d7ad8d6810b58df251da61b0af7
Clodonil <clodonil@gmail.com>
2019-03-11 23:09:45 -0300

    ajuste

--- bolt/modules/app/files/app/__init__.py
+++ bolt/modules/app/files/app/__init__.py
@@ -5,7 +5,7 @@ app.config.from_object('config')
 
 @app.route('/')
 def message():
-    message = app.config['ENV']
+    message = app.config['MSG']
     return render_template('page.html',message=message,db_url=app.config['DB_URL'], db_user=app.config['DB_USER'],db_passwd=app.config['DB_PASSWD'])
     
     
--- bolt/modules/app/files/config.py
+++ bolt/modules/app/files/config.py
@@ -1,6 +1,6 @@
 import os
 
-ENV        = 'development'
+MSG        = 'development'
 DB_URL     = 'db_url'
 DB_USER    = 'db_user'
 DB_PASSWD  = 'db_passwd'
